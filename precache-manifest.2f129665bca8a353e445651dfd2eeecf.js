3e4a1bba68177ffa57ec3850798b9edfbd49964d
Konstantin <47443235+SemenovKS@users.noreply.github.com>
2019-03-15 12:09:15 +0300

    Updates

---
+++ precache-manifest.2f129665bca8a353e445651dfd2eeecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5abbd0173d973bc4938d",
+    "url": "/TestApp/static/js/runtime~main.5abbd017.js"
+  },
+  {
+    "revision": "0bd63bcee4082bdbe221",
+    "url": "/TestApp/static/js/main.0bd63bce.chunk.js"
+  },
+  {
+    "revision": "2606b1e59674e48645e0",
+    "url": "/TestApp/static/js/1.2606b1e5.chunk.js"
+  },
+  {
+    "revision": "0bd63bcee4082bdbe221",
+    "url": "/TestApp/static/css/main.56427565.chunk.css"
+  },
+  {
+    "revision": "36eda5efcbfb14cebba53c7849d340c6",
+    "url": "/TestApp/index.html"
+  }
+];
